151678cc33b000dc1730d0cef44afc08b066202d
pierrealixt <pierrealix@icloud.com>
2020-02-14 09:32:00 +0200

    Sign in / Sign up / Log out

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200130162612) do
+ActiveRecord::Schema.define(version: 2020_02_14_071057) do
 
   create_table "boxes", force: :cascade do |t|
     t.integer "pos_x"
@@ -99,8 +99,16 @@ ActiveRecord::Schema.define(version: 20200130162612) do
     t.index ["item_id"], name: "index_nursery_items_on_item_id"
   end
 
-# Could not dump table "plots" because of following StandardError
-#   Unknown type 'json' for column 'geometry'
+  create_table "plots", force: :cascade do |t|
+    t.string "name"
+    t.integer "length_cm"
+    t.integer "width_cm"
+    t.integer "garden_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.json "geometry"
+    t.index ["garden_id"], name: "index_plots_on_garden_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.integer "item_id"
@@ -125,6 +133,18 @@ ActiveRecord::Schema.define(version: 20200130162612) do
     t.string "options"
     t.integer "item_id"
     t.index ["item_id"], name: "index_spacings_on_item_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
